92b1d9a6e330612346d05d441a4ba35051592ac4
computgeeks <142043410+computgeeks@users.noreply.github.com>
2023-10-13 16:20:41 -0700

    Add files via upload

---
+++ 1v1.lol/js/unityUrls.js
@@ -0,0 +1,10 @@
+window.loadedUrls = true;
+
+function getBuildUrl() {
+	return "https://justbuild.nyc3.cdn.digitaloceanspaces.com/NewCI/1v1/Prod/140/d463e74dc1404e9a81fa22946275eeec";
+}
+
+function getGameLoaderUrl() {
+	return getBuildUrl() + "/WebGL.loader.js";
+}
+
